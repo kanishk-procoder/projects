27405c9ef5b9a543fa8468b17505694e85287838
KANISHK GARG <kanishkgargggc@gmail.com>
2025-06-22 23:50:11 +0530

    Initial commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random
+options = ["rock", "paper", "scissor"]
+print(" rock paper scissors game ".center(50,"="))
+count_win = 0
+count_lose = 0
+while True:
+    choice = input("enter your choice : ")
+    n = random.choice(options)
+
+    if(choice.lower() == "done"):
+        break
+
+    elif choice.lower() not in options:
+        print("invalid choice \n")
+
+    elif(choice.lower() == n):
+        count_win += 1
+        print("you won")
+        print(f"win = {count_win} \n lose = {count_lose} \n")
+
+    elif(choice.lower() != n):
+        count_lose += 1
+        print("you lost")
+        print(f"win = {count_win} \nlose = {count_lose} \n")
